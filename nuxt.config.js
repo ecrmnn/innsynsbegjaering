ec74b192800b70c393b84315e566859348501c22
ecrmnn <danieleckermann@gmail.com>
2019-12-15 16:24:56 +0100

    Adds open graph meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,12 +14,56 @@ module.exports = {
         name: 'description',
         content: 'Du har rett til innsyn i dine data. Vi hjelper deg med innsynsbegjæring på 1, 2, 3!',
       },
+      {
+        hid: 'og:locale',
+        name: 'og:locale',
+        content: 'nb_NO',
+      },
+      {
+        hid: 'og:type',
+        name: 'og:type',
+        content: 'website',
+      },
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'Innsynsbegjæring.no',
+      },
+      {
+        hid: 'og:title',
+        name: 'og:title',
+        content: 'Innsynsbegjæring.no',
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Du har rett til innsyn i dine data. Vi hjelper deg med innsynsbegjæring på 1, 2, 3!',
+      },
+      {
+        hid: 'og:url',
+        name: 'og:url',
+        content: 'https://www.xn--innsynsbegjring-7lb.no',
+      },
+      {
+        hid: 'og:site_name',
+        name: 'og:site_name',
+        content: 'Innsynsbegjæring.no',
+      },
+      {
+        hid: 'og:image',
+        name: 'og:image',
+        content: 'https://www.xn--innsynsbegjring-7lb.no/og.gif',
+      },
     ],
     link: [
       {
         rel: 'icon',
         type: 'image/x-icon',
         href: '/favicon.ico',
+      },
+      {
+        rel: 'canonical',
+        href: 'https://www.xn--innsynsbegjring-7lb.no',
       },
     ],
   },
